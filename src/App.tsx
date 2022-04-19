59d37b4225210640f0121f33af1da2ee270c2d83
sandream <fs517010856@gmail.com>
2022-04-19 20:07:42 +0800

    导航栏布局初步完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
+import { match } from 'assert'
+import React, { useEffect } from 'react'
+import { Link, Route, Switch, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Home = () => (
   <div>
@@ -18,28 +20,67 @@ const Products = () => (
     <h2>Products</h2>
   </div>
 )
+const NoMatch = () => {
+  let location = useLocation()
 
-export default function App() {
   return (
     <div>
-      <nav className="navbar navbar-light">
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  )
+}
+const Wrapper = styled.div`
+  border: 1px solid red;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Nav = styled.nav`
+  border: 1px solid black;
+  > ul {
+    display: flex;
+    > li {
+      line-height: 30px;
+      text-align: center;
+      width: 33.3333%;
+      padding: 10px;
+    }
+  }
+`
+const Main = styled.main`
+  border: 1px solid green;
+  flex: 1;
+`
+export default function App() {
+  return (
+    <Wrapper>
+      <Main>
+        <Switch>
+          <Route exact path="/tags" component={Home}></Route>
+          <Route path="/money">
+            <Category />
+          </Route>
+          <Route path="/statistics" component={Products}></Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Main>
+      <Nav>
         <ul className="nav navbar-nav">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/tags">标签</Link>
           </li>
           <li>
-            <Link to="/category">Category</Link>
+            <Link to="/money">记一笔</Link>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <Link to="/statistics">统计</Link>
           </li>
         </ul>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/category" component={Category}></Route>
-        <Route path="/products" component={Products}></Route>
-      </Switch>
-    </div>
+      </Nav>
+    </Wrapper>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './index.scss'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
