a6df3e25f5386c342495f488d05c9990df2c6856
sandream <fs517010856@gmail.com>
2022-04-19 15:13:51 +0800

    添加react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
-import './App.scss'
-import styled from 'styled-components'
+import { Link, Route, Switch } from 'react-router-dom'
 
-const Button = styled.button`
-  line-height: 40px;
-  color: red;
-`
-function App() {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+)
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
+
+export default function App() {
   return (
-    <div className="App">
-      <Button>fan</Button>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/category" component={Category}></Route>
+        <Route path="/products" component={Products}></Route>
+      </Switch>
     </div>
   )
 }
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-import App from './App'
+// src/index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 )
